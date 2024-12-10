75ff1affb6faa3060a60b47b58aeef6a1dc8f0c7
muni <munichandrathunga@gmail.com>
2024-12-10 17:52:53 +0530

    sivayya

---
+++ src/test/java/com/amazon/authservice/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
